0c59d0427e658723c85d6cd5a604f8860e381e15
mario welch <mario.welch01@gmail.com>
2022-02-16 09:09:16 -0600

    completed unique username, individual pages, and user ad profile

---
+++ src/main/java/com/codeup/adlister/controllers/IndividualAdServlet.java
@@ -0,0 +1,23 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ad")
+public class IndividualAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long id = Long.parseLong(request.getParameter("id"));
+
+        Ad adInfo = DaoFactory.getAdsDao().individualAdID(id);
+        request.setAttribute("adInfo", adInfo);
+
+        request.getRequestDispatcher("/WEB-INF/ads/individual.jsp").forward(request, response);
+    }
+}
