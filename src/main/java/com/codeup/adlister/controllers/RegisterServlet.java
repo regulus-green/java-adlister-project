4aecb36bd126756a84306fcda1affca407631362
michael galimore <michael.galimore1@gmail.com>
2022-02-17 16:11:01 -0600

    navbar and error messages

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
@@ -23,19 +24,44 @@ public class RegisterServlet extends HttpServlet {
         String passwordConfirmation = request.getParameter("confirm_password");
 
         // validate input
-        boolean inputHasErrors = username.isEmpty()
-            || email.isEmpty()
-            || password.isEmpty()
-            || (! password.equals(passwordConfirmation));
-
-        if (inputHasErrors) {
-            response.sendRedirect("/register");
-            return;
+        boolean inputHasErrors = false;
+        if (username.isEmpty()) {
+            request.getSession().setAttribute("error", "Please enter a username.");
+            inputHasErrors = true;
+        } else if (DaoFactory.getUsersDao().findByUsername(username) != null) {
+            request.setAttribute("userError", "That user name is already in use");
+            inputHasErrors = true;
+        }
+        if (email.isEmpty()) {
+            request.getSession().setAttribute("error", "Please enter an email.");
+            inputHasErrors = true;
+        }
+        if (password.isEmpty()) {
+            request.getSession().setAttribute("error", "Please enter in a password for your account.");
+            inputHasErrors = true;
+        } else if (!password.equals(passwordConfirmation)) {
+            request.getSession().setAttribute("error", "Your passwords do not match. Please re-enter.");
+            inputHasErrors = true;
         }
 
-        // create and save a new user
-        User user = new User(username, email, password);
-        DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
+
+        if (inputHasErrors) {
+            request.setAttribute("username", username);
+            request.setAttribute("email", email);
+            try {
+                request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        } else { // if everything was fine, let's create the user and add them to database
+            User user = new User(username, email, password);
+            DaoFactory.getUsersDao().insert(user);
+            response.sendRedirect("/login");
+
+            // create and save a new user
+//        User user = new User(username, email, password);
+//        DaoFactory.getUsersDao().insert(user);
+//        response.sendRedirect("/login");
+        }
     }
 }
--- src/main/webapp/WEB-INF/partials/navbar.jsp
+++ src/main/webapp/WEB-INF/partials/navbar.jsp
@@ -1,13 +1,25 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <a class="navbar-brand" href="/ads">Adlister</a>
         </div>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="/login">Login</a></li>
-            <li><a href="/logout">Logout</a></li>
-        </ul>
+        <c:choose>
+            <c:when test="${sessionScope.user != null}">
+                <li><a href="/logout">Logout</a></li>
+                <li><a href="/profile">Profile</a></li>
+                <li><a href="/ads">View Ads</a></li>
+                <li><a href="/ads/create">Create Ad</a></li>
+            </c:when>
+            <c:otherwise>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Register</a></li>
+            </c:otherwise>
+        </c:choose>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
+
+
+
